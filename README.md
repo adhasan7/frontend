152367a798e276db998047ff8cbc945620da6bcc
adhasan7 <adsansapu@gmail.com>
2023-06-16 16:26:58 +0700

    upload

--- README.md
+++ README.md
@@ -2,13 +2,5 @@
 
 #Lesson
 video 4
-1.Introduction 
-2. Install Tools 
-3. Create React-App
+1.Introduction 2. Install Tools 3. Create React-App
 4.Create Git Repository
-
-video 5
-1.create product array
-2.add product images
-3.render products
-4.style products
--- src/App.js
+++ src/App.js
@@ -1,32 +1,13 @@
-import data from "./data";
-//import './App.css';
+import React from "react";
 
 function App() {
   return (
     <div>
       <header>
-        <a href='/'>Amazon</a>
+        <a href='/'>amazone</a>
       </header>
       <main>
-        <h1>Featur product</h1>
-        <div className='products'>
-          {data.products.map((product) => (
-            <div className='product' key={product.slug}>
-              <a href={`/product/${product.slug}`}>
-                <img src={product.image} alt={product.name} />
-              </a>
-              <div className='product-info'>
-                <a href={`/product/${product.name}`}>
-                  <p>{product.name}</p>
-                </a>
-                <p>
-                  <strong>${product.price}</strong>
-                </p>
-                <button>add to card</button>
-              </div>
-            </div>
-          ))}
-        </div>
+        <h1>Featured Product</h1>
       </main>
     </div>
   );
